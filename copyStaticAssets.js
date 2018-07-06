e65f9879655b9541111fb6c146da9fc0647c630d
Pho Huynh <shortgiraffe4@gmail.com>
2018-07-06 10:05:58 +0700

    Add loader for buttons and pages

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -2,3 +2,4 @@ var shell = require('shelljs');
 
 shell.cp("-R", "static-pages/fonts", "static-pages/dist/");
 shell.cp("-R", "static-pages/js", "static-pages/dist/");
+shell.cp("-R", "static-pages/images", "static-pages/dist/");
